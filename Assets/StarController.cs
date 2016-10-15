0c8ed25da26130bf015ea0c9523f25bc44d3d9bf
orz025846 <ec_mail02@yahoo.co.jp>
2016-10-15 17:54:35 +0900

    オリジナルのゲームアプリを作ろう／ ゲームをストアへ公開しよう／ 【最終課題】オリジナルゲームアプリを開発しストアに公開しよう
    Lesson9課題の質問用です。（仮）制限時間内にオブジェクトをタップしてポイントを加算するゲーム制作途中です。＜質問：生成範囲の設定＞

---
+++ Assets/StarController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarController : MonoBehaviour {
+    // 移動速度のランダム変数用配列
+    static public float[] speed = { -20.0f, -5.0f };
+
+    // 小星の移動速度
+    static public float[] speedSs = { Random.Range(speed[0], speed[1]), Random.Range(speed[0], speed[1]), Random.Range(speed[0], speed[1]) };
+    
+    // オブジェクトの消滅位置
+    public float deadLine = -175;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // 小星を移動させる
+        transform.Translate(speedSs[0], speedSs[1], speedSs[2]);
+
+        // feldを透過したらオブジェクト消滅
+        if (transform.position.y < this.deadLine)
+        {
+            Destroy(gameObject);
+        } 
+	}
+}
---
+++ Assets/StarGenerator.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarGenerator : MonoBehaviour {
+    // smallStarのPrefab
+    public GameObject smallStarPrefab;
+
+    // smallStarの生成位置
+    //public Vector3 genPos = new Vector3(900.0f, 500.0f, 900.0f);
+    public float genPosX = 900;
+    public float genPosY = 500;
+    public float genPosZ = 900;
+
+    // smallStarの生成個数の上限
+    private int maxsSNum = 8;
+
+    // Use this for initialization
+    void Start () {
+
+        // 生成するPrefab数をランダムに決める
+        int n = Random.Range(1, maxsSNum + 1);
+        // 生成
+        for (int i = 0; i < n; i++)
+        {
+            GameObject smallStar = Instantiate(smallStarPrefab) as GameObject;
+            smallStar.transform.position = new Vector3(genPosX + i, genPosY * i, genPosZ * i);
+        }
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
