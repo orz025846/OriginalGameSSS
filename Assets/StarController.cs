dcf9032e2e586a1d791d6dbf18e56ef4b7aed159
orz025846 <ec_mail02@yahoo.co.jp>
2016-10-16 18:19:12 +0900

    オリジナルのゲームアプリを作ろう／ ゲームをストアへ公開しよう／ 【最終課題】オリジナルゲームアプリを開発しストアに公開しよう
    Lesson9課題の質問用です。調整中）StarGenerator:続・小星の落下位置、StarContoroller：衝撃判定（Terrainへの落下でパーティクルONにしたい）

--- Assets/StarController.cs
+++ Assets/StarController.cs
@@ -1,19 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.EventSystems;
 
 public class StarController : MonoBehaviour
 {
     // 移動速度のランダム変数用配列
-    public float[] speed = { -20.0f, -5.0f };
+    public float[] speed = { -2.0f, -0.05f };
 
     // 小星の移動速度
     public float[] speedSs;
-    
+
     // オブジェクトの消滅位置
     public float deadLine = -175;
 
     // Use this for initialization
-    void Start ()
+    void Start()
     {
         speedSs = new float[] {
             Random.Range (speed [0], speed [1]),
@@ -21,21 +22,27 @@ public class StarController : MonoBehaviour
             Random.Range (speed [0], speed [1])
         };
     }
-    
+
     // Update is called once per frame
-    void Update ()
+    void Update()
     {
         // 小星を移動させる
-        transform.Translate (speedSs [0], speedSs [1], speedSs [2]);
+        transform.Translate(speedSs[0], speedSs[1], speedSs[2]);
 
         // feldを透過したらオブジェクト消滅
-        if (transform.position.y < this.deadLine) {
-            Destroy (gameObject);
-        } 
+        if (transform.position.y < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
     }
 
     void OnTriggerEnter(Collider other)
     {
+        //Field以外に衝突した場合
+        if (other.gameObject.tag == "SmallStarTag")
+        {
+            return;
+        }
         GetComponent<ParticleSystem>().Play();
     }
 
--- Assets/StarGenerator.cs
+++ Assets/StarGenerator.cs
@@ -1,22 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class StarGenerator : MonoBehaviour
 {
     // smallStarのPrefab
     public GameObject smallStarPrefab;
+    /// <summary>
+    /// smallStarの生成位置
+    /// </summary>
+    [SerializeField]
+    private float genPosZ = 500;
 
-    // smallStarの生成位置
-    //>public Vector3 genPos = new Vector3(-690.0f, -170.0f, 259.0f);
-    public float genPosZ = 259;
-
-    // smallStarの生成個数の上限
+    /// <summary>
+    /// smallStarの生成個数の上限
+    /// </summary>
     private int maxsSNum = 8;
-
+    /// <summary>
+    /// Terrainの位置を格納する変数 
+    /// </summary>
     private Vector3 fieldPosition;
+    /// <summary>
+    /// 左下の位置 ~ 右上の位置 
+    /// </summary>
     private float minPosX = 0.0f;
-    private float minPosY = 0.0f;
+    private float maxPosX = 0.0f;
+    private float minPosZ = 0.0f;
+    private float maxPosZ = 0.0f;
+    /// <summary>
+    /// Trrainの一辺の長さ 
+    /// </summary>
     private float terrainLength = 1500.0f;
+    /// <summary>
+    ///何秒に１度Starを出現させるか 
+    /// </summary>
+    private float timeInterval = 2.0f;
+    /// <summary>
+    /// 秒数をカウントするカウンター 
+    /// </summary>
+    private float timeCounter = 0;
 
     // Use this for initialization
     void Start()
@@ -25,32 +47,43 @@ public class StarGenerator : MonoBehaviour
         //追記ぶん(Fieldオブジェクトの取り込み）
         GameObject field = GameObject.Find("Field") as GameObject;
         fieldPosition = field.transform.position;
-        Debug.Log(fieldPosition);
         //端の位置を計算
-        minPosX = fieldPosition.x + terrainLength / 2;
-        //minPosY = fieldPosition.y - terrainLength / 2;
-
-        //惑星が全て収まるように、最大個数のときの間隔を計算
-        float interval = terrainLength / maxsSNum;
-
-        // 生成するPrefab数をランダムに決める
-        int n = Random.Range(1, maxsSNum + 1);
-        // 生成
-        for (int i = 0; i < n; i++)
-        {
-            //星の生成
-            GameObject smallStar = Instantiate(smallStarPrefab) as GameObject;
-            smallStar.transform.position = new Vector3(minPosX + interval * i, minPosY + interval * i, genPosZ * i);
-            //変更ぶん
-            //smallStar.transform.position = new Vector3(minPosX * interval * i, minPosY * interval * i, genPosZ * i);
-            
-        }
+        //左下のX座標
+        minPosX = fieldPosition.x;
+        //右上のX座標
+        maxPosX = fieldPosition.x + terrainLength;
+        //左下のZ座標
+        minPosZ = fieldPosition.z;
+        //右上のZ座標
+        maxPosZ = fieldPosition.z + terrainLength;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        //秒数をカウント
+        timeCounter += Time.deltaTime;
+        //一定秒数を超えたらtrueになる
+        if (timeCounter > timeInterval)
+        {
+            //秒数カウンターを0で初期化
+            timeCounter = 0;
+            // 生成するPrefab数をランダムに決める
+            int n = Random.Range(1, maxsSNum + 1);
+            // 生成
+            for (int i = 0; i < n; i++)
+            {
+                //星の生成
+                GameObject smallStar = Instantiate(smallStarPrefab) as GameObject;
+                //位置を指定
+                smallStar.transform.position =
+                    new Vector3(
+                    Random.Range(minPosX, maxPosX),
+                    genPosZ,
+                    Random.Range(minPosZ, maxPosZ)
+                );
+            }
+        }
     }
 
 }
