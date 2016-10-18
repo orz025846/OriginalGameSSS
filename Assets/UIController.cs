9d5d69f03a4442b69a4fc174ce177b9d3891c11e
orz025846 <ec_mail02@yahoo.co.jp>
2016-10-18 20:49:11 +0900

    オリジナルのゲームアプリを作ろう／ ゲームをストアへ公開しよう／ 【最終課題】オリジナルゲームアプリを開発しストアに公開しよう
    Lesson9課題の質問用です。調整）UIController：変数初期化、他随時調整中

---
+++ Assets/UIController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour {
+    private float startTime = 15.00f;
+    private float time;
+    private float overTime = 0;
+    private float timeflat;
+    private GameObject timeWatchText;
+    private GameObject timeUPText;
+
+    // ゲーム終了の判定
+    private bool isEnd = false;
+
+    // Use this for initialization
+    void Start ()
+    {
+        this.time = 0;
+        this.timeflat = 0;
+        this.timeWatchText = GameObject.Find("TimeWatchText");
+        this.timeUPText = GameObject.Find("TimeUPText");
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        this.timeflat += Time.deltaTime;
+        this.time = startTime - timeflat;
+        this.timeWatchText.GetComponent<Text>().text = time.ToString("F2");
+
+        if(time <= overTime)
+        {
+            this.time = 0;
+            this.timeflat = 0;
+            this.timeUPText.GetComponent<Text>().text = "TimeUP";
+        }
+    }
+}
