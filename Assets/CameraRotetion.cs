9b8e2ae18a3cd163a7d68f76cc9af7f02feca3a9
orz025846 <ec_mail02@yahoo.co.jp>
2016-10-25 20:59:09 +0900

    オリジナルのゲームアプリを作ろう／ ゲームをストアへ公開しよう／ 【最終課題】オリジナルゲームアプリを開発しストアに公開しよう
    Lesson9課題の質問用です。調整）カメラ、プロジェクト内保存失敗につき修正中

---
+++ Assets/CameraRotetion.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRotetion : MonoBehaviour {
+
+    private Vector3 mPos; //マウスカーソルの位置
+    private float cameraRot = 10.0f;  //カメラの移動量
+
+	// Use this for initialization
+	void Start () {
+
+        Debug.Log(this.gameObject.transform.position);
+        Debug.Log(Camera.main.transform.position);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // カーソルの位置座標
+        mPos = Input.mousePosition;
+        
+            // カーソル位置が中心より右＞＞右に移動
+       
+
+            // カーソル位置が中心より左＞＞左に移動
+        	
+	}
+}
