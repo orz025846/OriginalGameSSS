8340c0732d73ff8c68b2b76264fb347725aba4c4
orz025846 <ec_mail02@yahoo.co.jp>
2016-10-29 19:27:48 +0900

    オリジナルのゲームアプリを作ろう／ ゲームをストアへ公開しよう／ 【最終課題】オリジナルゲームアプリを開発しストアに公開しよう
    Lesson9課題の質問用です。調整）オブジェクト制御系.cs（StarOnTrigger/PumpkinController/他）、CameraRotetion＞OnMouseButton

--- Assets/StarController.cs
+++ Assets/StarController.cs
@@ -4,19 +4,6 @@ using System.Collections;
 
 public class StarController : MonoBehaviour
 {
-    // 生成オブジェクトのアタッチ
-    [SerializeField]
-    private GameObject smallStarPrefab;
-    [SerializeField]
-    private GameObject pumpkin_02Prefab;
-
-    // オブジェクトの移動速度
-    public float[] speed = { -50.0f, -50.0f };  //ランダム変数
-    public float[] speedSs;  //移動速度用
-
-    // オブジェクトの消滅位置（y座標に設定）
-    public float deadLine = -175;
-
     // ポイント加算用UIのアタッチ
     [SerializeField]
     public Image image;
@@ -24,14 +11,6 @@ public class StarController : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        float aaa = Time.deltaTime;
-        float bbb = Time.deltaTime * 0.2f;
-        //float ccc = Time.deltaTime - 0.2f;
-        speedSs = new float[] {
-            Random.Range (speed [0] * aaa, speed [1] * aaa),
-            Random.Range (speed [0] * bbb, speed [1] * bbb),
-            Random.Range (speed [0] * bbb, speed [1] * bbb)
-        };
 
     }
 
@@ -62,14 +41,5 @@ public class StarController : MonoBehaviour
         }
 
 
-        // field（ｙ座標）を透過したらオブジェクト消滅
-        if (transform.position.y < this.deadLine)
-        {
-            //１秒後にDestroy
-            Destroy(gameObject, 1.0f);
-        }
-        // オブジェクトを移動させる
-        transform.Translate(speedSs[0], speedSs[1], speedSs[2]);
-    }
-
+        
 }
