9c81e3890fe1aa1ffa6f2b6bec41475dc211f44e
orz025846 <ec_mail02@yahoo.co.jp>
2016-12-06 21:52:00 +0900

    オリジナルのゲームアプリを作ろう／ ゲームをストアへ公開しよう／ 【最終課題】オリジナルゲームアプリを開発しストアに公開しよう
    Lesson9課題の質問用最終コミットです。変更）PBar.cs他

---
+++ Assets/App/Game/Script/ObjController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ObjController : MonoBehaviour
+{
+	// ポイント加算用UIのアタッチ
+	[SerializeField]
+	public PBar pBar;
+	// ゲームクリア表示用テキスト
+	public GameObject clearText;
+
+
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		// バーの初期化
+		this.pBar.Initialize ();
+		// クリア表示用テキストの取得（TimeUPTextレンタル）
+		this.clearText = GameObject.Find ("TimeUPText");
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		// Raycastの実装
+		if (Input.GetMouseButtonDown (0)) {  // マウス左クリック
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);  //Ray発射
+			RaycastHit hit;  //Rayが当たったオブジェクト情報取得用
+			float maxDistance = 2000;  //Ray軌跡の長さ
+
+			if (Physics.Raycast (ray, out hit, Mathf.Infinity)) {
+				pBar.AddValue (0.1f);
+				Destroy (hit.collider.gameObject, 0.1f);
+
+			}
+
+			if (pBar.FillAmount >= 1) {  //バーが満杯
+				this.clearText.GetComponent<Text> ().text = "Full Charge!!";
+				StartCoroutine (SegueGameClearScene ()); //クリア画面に遷移
+			}
+
+
+			Debug.DrawRay (ray.origin, ray.direction * maxDistance, Color.green, 5, false);
+		}
+	}
+
+	// Scean遷移用コルーチンより呼び出し（待機時間の制御）
+	private IEnumerator SegueGameClearScene ()
+	{
+		yield return new WaitForSeconds (3.0f);
+		SceneManager.LoadScene ("Clear");
+	}
+
+        
+}
---
+++ Assets/App/Game/Script/PBar.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PBar : MonoBehaviour
+{
+	[SerializeField]
+	private Image progressImage;
+
+	public float FillAmount {
+		get {
+			return progressImage.fillAmount;
+		}
+	}
+
+	public void Initialize ()
+	{
+		progressImage.fillAmount = 0;
+	}
+
+	/// <summary>
+	/// Sets the value.
+	/// </summary>
+	/// <param name="value">Value.</param>
+	public void AddValue (float value)
+	{
+		//ゲージを変化させる
+		progressImage.fillAmount += value;
+		//２０％以下の時
+		if (FillAmount <= 0.2f) {
+			progressImage.color = Color.red;
+		} else if (0.2f <= FillAmount && FillAmount <= 0.4f) {
+			progressImage.color = Color.yellow;
+		}
+	}
+}
