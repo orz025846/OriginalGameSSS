a75e31768929b79dc9ae2e9998ae66240e52e5d9
orz025846 <ec_mail02@yahoo.co.jp>
2016-12-20 23:45:31 +0900

    オリジナルのゲームアプリを作ろう／ ゲームをストアへ公開しよう／ 【最終課題】オリジナルゲームアプリを開発しストアに公開しよう
    Lesson9課題の質問用最終コミットです。調整＞Start画面の装飾、色味、ゲームレベル

---
+++ Assets/App/Start/Script/RotateUI.cs
@@ -0,0 +1,17 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using System.Collections;
+
+public class RotateUI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        transform.DOBlendableRotateBy(new Vector3(-360, 90, 90), 1.0f).SetLoops(-1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
